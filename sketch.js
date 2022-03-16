457a096f68b371a7891bdae83b24123b965d56e7
cjf8140 <32036089+cjf8140@users.noreply.github.com>
2022-03-16 23:50:47 +0900

    as

--- sketch.js
+++ sketch.js
@@ -1,22 +1,28 @@
-function preload() {
-  img = loadImage("img.jpeg");
-  // img = loadImage("img2.png");
-}
+// function preload() {
+//   img = loadImage("img.jpeg");
+//   img = loadImage("img2.png");
+// }
 
-const plt = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-const plt2 = "@%#*+=-:. "
+const plt = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\)1}]?-_+~>i!lI;:,^`'.                                                                                             "
+
+let img;
+let asciiDiv;
 function setup() {
   noCanvas();
+  img = createCapture(VIDEO);
+  img.size(32,32);
+  
+  asciiDiv=createDiv();
+}
+function draw() {
   // createCanvas(360, 360);
-  background(0);
   // image(img, 0, 0, width, height);
-  
+
   const w = width/img.width;
-  const itv = 7;
+  const itv = 1;
   img.loadPixels();
-  
+  let asciiImage = '';
   for (let j=0; j < img.width; j+=itv) {
-    let row = "";
     for (let i=0; i < img.width; i+=itv) {
       const pixelIndex = (i + j * img.height) * 4;
       //first cell of each pixels
@@ -31,17 +37,17 @@ function setup() {
         avr = 255;
       }
       
-      noStroke();
-      fill(255);
-      textSize(w*itv);
+      // noStroke();
+      // fill(255);
+      // textSize(w*itv);
       
       let chr = plt[floor(map(avr,0,256, 0, plt.length))];
       textAlign(CENTER, CENTER);
       text(chr, i*w, j*w);
-      if(chr == ' ') row+= "&nbsp;";  //빈공간 불침범스페이스로
-      else row+=chr;
+      if(chr == ' ') asciiImage+= "&nbsp;";  //빈공간 불침범스페이스로
+      else asciiImage+=chr;
     }
-    createDiv(row);
-    console.log(row);
+    asciiImage+='<br/>';
   }
+  asciiDiv.html(asciiImage);
 }
